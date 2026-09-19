0a34f873d9821bee36a0a22f7b38bcb9d066d438
abdo0123110335-cmd <abdo0123110335@gmail.com>
2026-09-19 12:31:06 +0200

    Remove duplicate import of provider package

--- lib/features/chart/chart_screen.dart
+++ lib/features/chart/chart_screen.dart
@@ -1,6 +1,6 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart' hide ChangeNotifierProvider;
 import 'package:provider/provider.dart' hide Provider;
-import 'package:provider/provider.dart';
 
 import 'package:binance_spot_pro/core/providers/core_providers.dart';
 import 'package:binance_spot_pro/core/theme/app_theme.dart';
